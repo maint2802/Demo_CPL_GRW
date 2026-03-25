a695d99d2f93b2b2dded6f0fa94354d19e4ff7b2
MaiNT <mai28022000@gmail.com>
2026-03-25 11:07:35 +0700

    add newTodo using useRef

--- src/components/TodoAdd.tsx
+++ src/components/TodoAdd.tsx
@@ -1,29 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import type { TodoAddProps } from "../types/types";
 
 const TodoAdd = ({ todolist, setTodolist }: TodoAddProps) => {
-  const [inputValue, setInputValue] = useState<string>("");
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const handleAddTodo = () => {
     // get input value
     // create newTodo follow format todo
+    if (!inputRef.current) return;
     const newTodo = {
-      name: inputValue,
+      name: inputRef.current.value,
       id: Date.now().toString(),
       completed: false,
     };
-    // todolist.push(newTodo)
-    // setTodolist(todolist)
+
     setTodolist([...todolist, newTodo]);
+    // spread
+    inputRef.current.value = "";
+    inputRef.current.focus();
   };
 
-  const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(e.target.value);
-  };
+  //   preState vs currentState
 
+  // ref arr vs obj
   return (
     <div>
-      <input type="text" onChange={handleChangeInput} />
+      <input type="text" ref={inputRef} />
       <button onClick={handleAddTodo}>add</button>
     </div>
   );
